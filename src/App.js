561d93df6ee816499bb24e7d55ae354dfb32c08b
Najaf <najafkhan6625@gmail.com>
2023-01-12 23:45:16 +0500

    level:1 Init project, level:2 map Notes data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Note from "./components/Note";
+import Footer from "./components/Footer";
+import Notes from "./components/data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {Notes.map((notesItem) => {
+        return (
+          <Note
+            key={notesItem.key}
+            title={notesItem.title}
+            content={notesItem.content}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
